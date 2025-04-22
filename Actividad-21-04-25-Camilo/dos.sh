06c458752071734e792d2702b512bdee1c5f4039
Camilo-6 <jcamilo@ciencias.unam.mx>
2025-04-22 08:35:10 -0600

    cambios sobre el manejo de los errores al usar ssh

--- Actividad-21-04-25-Camilo/dos.sh
+++ Actividad-21-04-25-Camilo/dos.sh
@@ -2,8 +2,7 @@
 
 archivo="revisar.csv"
 usuario="yankees"
-timeout=20
-private_key_path="~/.ssh/id_rsa"
+timeout=10
 
 # Leer todos los hosts en una lista
 mapfile -t hosts < "$archivo"
@@ -21,8 +20,7 @@ for host in "${hosts[@]}"; do
     echo "Intentando conectar como '$usuario' a host '$host' (esperando hasta ${timeout}s)..."
 
     # Intentar conectarse y ejecutar los comandos guardando la salida
-    ssh -i "$private_key_path" \
-        -o ConnectTimeout=$timeout \
+    ssh -o ConnectTimeout=$timeout \
         -o PasswordAuthentication=no \
         -o BatchMode=yes \
         "$usuario@$host" \
@@ -30,7 +28,18 @@ for host in "${hosts[@]}"; do
 
     # Verificar si la conexion tuvo exito
     if [[ $? -ne 0 ]]; then
-        echo "No se pudo conectar con $usuario@$host (apagado, inaccesible o sin clave publica autorizada)"
+        # Revisar si ssh_output.log contiene errores relacionados con lo que salio mal
+        if grep -q "Connection timed out" ssh_output.log; then
+            echo "No se pudo conectar con $usuario@$host (timeout)"
+        elif grep -q "No route to host" ssh_output.log; then
+            echo "No se pudo conectar con $usuario@$host (sin ruta al host)"
+        elif grep -q "Connection refused" ssh_output.log; then
+            echo "No se pudo conectar con $usuario@$host (conexion rechazada)"
+        elif grep -q "Permission denied" ssh_output.log; then
+            echo "No se pudo conectar con $usuario@$host (permiso denegado)"
+        else
+            echo "No se pudo conectar con $usuario@$host (error desconocido)"
+        fi
     else
         # Revisar si se pidio la contrasenia
         if grep -q "password:" ssh_output.log; then
@@ -42,11 +51,6 @@ for host in "${hosts[@]}"; do
             echo "Tiempo de actividad:"
             grep "up" ssh_output.log
         fi
-    fi
-
-    # Verificar si ssh_output.log contiene errores relacionados con la inaccesibilidad del host
-    if grep -q "Connection timed out" ssh_output.log || grep -q "No route to host" ssh_output.log || grep -q "Connection refused" ssh_output.log; then
-        echo "La maquina $host no esta disponible o inaccesible"
     fi
 
     echo "-------------------------------------"
